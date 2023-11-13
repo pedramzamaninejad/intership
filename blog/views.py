f5b89e141a2f5fd11b44cc89a615f4a6bd1ef6c2
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2023-11-13 12:18:33 +0330

    create comment form in blog detail

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-
+from django.contrib.auth.decorators import login_required
 
 from .models import Blog
+from .forms import CommentForm, BlogForm
 
 
 class BlogListView(generic.ListView):
@@ -11,6 +12,30 @@ class BlogListView(generic.ListView):
     context_object_name = 'blogs'
 
 
+@login_required
 def blog_detail_view(request, pk):
+    # blog object
     blog = get_object_or_404(Blog, pk=pk)
-    return render(request, 'blog/blog_detail.html', {'blog': blog})
+    # blog comments
+    blog_comment = blog.comment.filter('rate')
+
+    # post a comment
+    if request.method == 'POST':
+        # create comment form object
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            # comment user and blog
+            new_comment = comment_form.save(commit=False)
+            new_comment.blog = blog
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'blog': blog,
+        'comments': blog_comment,
+        'comment_form': comment_form
+    }
+    return render(request, 'blog/blog_detail.html', context=context)
